2e7f89b2a1a46b9139a98d465d5637b6b1b57b00
Billy Joel <105961160+joel6277@users.noreply.github.com>
2022-11-24 00:39:46 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-function esconderBandera(nombre){
+function esconderBandera(nombre) {
     document.getElementById(nombre).style.display = 'none'
 }
 
-function ocultarBanderas(){
+function ocultarBanderas() {
     for (let i = 1; i <= 5; i++) {
-        esconderBandera('ejercicio'+i)
+        esconderBandera('ejercicio' + i)
     }
 }
 
-function mostrarBandera(nombre){
+function mostrarBandera(nombre) {
     document.getElementById(nombre).style.display = 'contents'
 }
 
-function selectorBandera(){
+function selectorBandera() {
     ocultarBanderas()
     let select = document.getElementById('select')
     mostrarBandera(select.value)
 }
 
 ocultarBanderas()
-let select = document.getElementById('select') 
+let select = document.getElementById('select')
 select.onchange = selectorBandera
 mostrarBandera(select.value)
